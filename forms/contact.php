69e9aff29524a93cc515e1e1d19556abd3c2cab7
Tobin786 <tmathew@upei.ca>
2023-03-01 20:49:49 -0400

    php-email

--- forms/contact.php
+++ forms/contact.php
@@ -1,136 +1,67 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
-require_once __DIR__ . '/vendor/autoload.php';
-$errors = [];
-$errorMessage = '';
-
-if (!empty($_POST)) {
-   $name = $_POST['name'];
-   $email = $_POST['email'];
-   $message = $_POST['message'];
-
-   if (empty($name)) {
-       $errors[] = 'Name is empty';
-   }
-
-   if (empty($email)) {
-       $errors[] = 'Email is empty';
-   } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-       $errors[] = 'Email is invalid'
-
-   }
+use PHPMailer\PHPMailer\Exception;
 
 
-   if (empty($message)) {
-       $errors[] = 'Message is empty';
-   }
+require '../mail/src/PHPMailer.php';
+require '../mail/src/SMTP.php';
+require '../mail/src/Exception.php';
 
-   if (!empty($errors)) {
-       $allErrors = join('<br/>', $errors);
-       $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
-   } else {
-       $mail = new PHPMailer();
+// Check if form was submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
+  
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $subject = $_POST["subject"];
+  $message = $_POST["message"];
+  $body = "<h2>New message from your website</h2>";
+  $body .= "<p><strong>Name:</strong> " . $name . "</p>";
+  $body .= "<p><strong>Email:</strong> " . $email . "</p>";
+  $body .= "<p><strong>Subject:</strong> " . $subject . "</p>";
+  $body .= "<p><strong>Message:</strong><br>" . nl2br($message) . "</p>";
+  
+  // Instantiate PHPMailer
+  $mail = new PHPMailer();
 
-       // specify SMTP credentials
+  // Set mailer to use SMTP
+  $mail->isSMTP();
+  try{
+  // Enable SMTP debugging
+  // 0 = off (for production use)
+  // 1 = client messages
+  // 2 = client and server messages
+  $mail->SMTPDebug = 0;  
+  $mail->Mailer='smtp';                                      
+  $mail->isSMTP();                                             
+  $mail->Host       = 'smtp.gmail.com';                     
+  $mail->SMTPAuth   = true;                              
+  $mail->Username   = 'youremail@gmail.com';                  
+  $mail->Password   = 'yourpass';                         
+  $mail->SMTPSecure = 'tls';                               
+  $mail->Port       = '587';   
+  $mail->IsHTML(TRUE);
+  $mail->setFrom($email, $name);
+  $mail->addAddress('youremail@gmail.com');
 
+  // Set email subject and message body
+  $mail->Subject = "Email: ".$email." ".$subject;
+  $mail->Body    = $body;
 
-       $mail->isSMTP();
-       $mail->Host = 'smtp.mailtrap.io';
-       $mail->SMTPAuth = true;
-       $mail->Username = 'your_smtp_username';
-       $mail->Password = 'your_smtp_password';
-       $mail->SMTPSecure = 'tls';
-       $mail->Port = 2525;
-       $mail->setFrom($email, 'Mailtrap Website');
-       $mail->addAddress('example@example.com', 'Me');
-       $mail->Subject = 'New message from your website';
+  // Send the email and check for errors
+  if (!$mail->send()) {
+    http_response_code(500);
+      echo "Mailer Error: " . $mail->ErrorInfo;
+  } else {
+    http_response_code(200);
+      echo "OK";
+  }
+}catch (Exception $e) { 
+  //echo "<script>alert('I am here');</script";
+  http_response_code(403);
+  error_log("Error message: " . error_get_last(), 0);
 
-       // Enable HTML if needed
-       $mail->isHTML(true);
-       $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", nl2br($message)];
-       $body = join('<br />', $bodyParagraphs);
-       $mail->Body = $body;
-       echo $body;
-
-       if($mail->send()){
-           header('Location: thank-you.html'); // Redirect to 'thank you' page. Make sure you have it
-       } else {
-
-           $errorMessage = 'Oops, something went wrong. Mailer Error: ' . $mail->ErrorInfo;
-       }
-
-   }
-
+  echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"; 
+} 
 }
-
 ?>
-
-<html>
-<body>
- <form action="/swiftmailer_form.php" method="post" id="contact-form">
-   <h2>Contact us</h2>
-   <?php echo((!empty($errorMessage)) ? $errorMessage : '') ?>
-   <p>
-     <label>First Name:</label>
-     <input name="name" type="text"/>
-   </p>
-   <p>
-     <label>Email Address:</label>
-     <input style="cursor: pointer;" name="email" type="text"/>
-   </p>
-   <p>
-     <label>Message:</label>
-     <textarea name="message"></textarea>
-   </p>
-   <p>
-     <input type="submit" value="Send"/>
-   </p>
- </form>
-
-
- <script src="//cdnjs.cloudflare.com/ajax/libs/validate.js/0.13.1/validate.min.js"></script>
-
- <script>
-
-     const constraints = {
-         name: {
-             presence: {allowEmpty: false}
-         },
-         email: {
-             presence: {allowEmpty: false},
-             email: true
-         },
-         message: {
-             presence: {allowEmpty: false}
-         }
-     };
-
-     const form = document.getElementById('contact-form');
-     form.addEventListener('submit', function (event) {
-         const formValues = {
-             name: form.elements.name.value
-             email: form.elements.email.value,
-             message: form.elements.message.value
-
-         };
-
-         const errors = validate(formValues, constraints);
-
-         if (errors) {
-           event.preventDefault();
-             const errorMessage = Object
-                 .values(errors)
-                 .map(function (fieldValues) {
-                     return fieldValues.join(', ')
-                 })
-                 .join("\n");
-             alert(errorMessage);
-
-         }
-
-     }, false);
-
- </script>
-</body>
-</html>
